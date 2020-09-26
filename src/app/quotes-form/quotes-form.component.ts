eff5264e56f3653c82d2666190aa2dd7cbee2101
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-09-26 13:12:01 +0300

    Adding a button to add a new Quote

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 
@@ -9,6 +9,11 @@ import { Quote } from '../quote';
 })
 export class QuotesFormComponent implements OnInit {
   newQuote = new Quote(0, "", "", "", "", new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit(): void {
