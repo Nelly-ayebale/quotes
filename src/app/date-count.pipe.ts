163a6627aa2b1e198993ca79e77c998743ed600a
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-09-26 18:00:28 +0300

    Added buttons for upvote and downvote

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,12 +8,9 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number {
     let today: Date = new Date();
     let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value - todayWithNoTime)
-
+    var dateDifference = Math.abs(value - todayWithNoTime);
     const secondsInDay = 86400;
     var dateDifferenceSeconds = dateDifference * 0.001;
-
-
     var dateCounter = dateDifferenceSeconds / secondsInDay;
 
     if (dateCounter >= 1 && value > todayWithNoTime) {
@@ -22,5 +19,4 @@ export class DateCountPipe implements PipeTransform {
       return 0;
     }
   }
-
 }
